fb571a375871f068bdf25ecbcb1b65d2f13cafc7
jl54 <jonas@jonasluethi.dev>
2025-12-26 22:17:54 +0100

    feat: Nvim transparent background

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,17 @@
-return { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup{
-            flavour = vim.g.catppuccin_flavour or "frappe"
-        }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = vim.g.catppuccin_flavour or "frappe",
+			transparent_background = true,
+            float = {
+                transparent = true,
+                solid = false
+            },
+		})
 
-        vim.cmd[[colorscheme catppuccin]]
-    end
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }
