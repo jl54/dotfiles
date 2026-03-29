babbb1315223267e86f8bbca5c8da8271d37edbf
jl54 <jonas@jonasluethi.dev>
2026-03-29 22:10:22 +0200

    fix(nvim): Fixes error when copying files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 vim.g.catppuccin_flavour = "macchiato"
+vim.g.netrw_keepdir = 0
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
