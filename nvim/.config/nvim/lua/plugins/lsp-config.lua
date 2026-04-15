bae3c4b92b3cc2a81a386eb9bd2a82cec7528c02
jl54 <jonas@jonasluethi.dev>
2026-04-16 00:22:51 +0200

    feat(nvim): Sets gopls default config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,72 +1,75 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"gopls",
-				"intelephense",
-				"vtsls",
-			},
-			automatic_installation = true,
-		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "gopls",
+                "intelephense",
+                "vtsls",
+            },
+            automatic_installation = true,
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.lsp.config("*", {
-				capabilities = capabilities,
-			})
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
 
-			local vue_language_server_path = vim.fn.stdpath("data")
-				.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+            local vue_language_server_path = vim.fn.stdpath("data")
+                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
-			local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-			local vue_plugin = {
-				name = "@vue/typescript-plugin",
-				location = vue_language_server_path,
-				languages = { "vue" },
-				configNamespace = "typescript",
-			}
+            local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+            local vue_plugin = {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" },
+                configNamespace = "typescript",
+            }
 
-			local vtsls_config = {
-				settings = {
-					vtsls = {
-						tsserver = {
-							globalPlugins = {
-								vue_plugin,
-							},
-						},
-					},
-				},
-				filetypes = tsserver_filetypes,
-			}
+            local vtsls_config = {
+                settings = {
+                    vtsls = {
+                        tsserver = {
+                            globalPlugins = {
+                                vue_plugin,
+                            },
+                        },
+                    },
+                },
+                filetypes = tsserver_filetypes,
+            }
 
-			local vue_ls_config = {}
-			vim.lsp.config("vtsls", vtsls_config)
-			vim.lsp.config("vue_ls", vue_ls_config)
-			vim.lsp.enable({ "vtsls", "vue_ls", "gopls", "tofu_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+            local gopls_config = {}
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            local vue_ls_config = {}
+            vim.lsp.config("vtsls", vtsls_config)
+            vim.lsp.config("vue_ls", vue_ls_config)
+            vim.lsp.config("gopls", gopls_config)
+            vim.lsp.enable({ "vtsls", "vue_ls", "gopls", "tofu_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
 
-			vim.diagnostic.config({
-				update_in_insert = true,
-				virtual_text = true,
-			})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+            vim.diagnostic.config({
+                update_in_insert = true,
+                virtual_text = true,
+            })
+        end,
+    },
 }
