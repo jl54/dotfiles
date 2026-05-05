a5c85ba0207cef287aa33d2e763464a5fc519684
jl54 <jonas@jonasluethi.dev>
2026-05-05 22:39:41 +0200

    fix: Treesitter config broken in neovim 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,52 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
-    opts = {
-        auto_install = true,
-        indent = { enable = true },
-        highlight = { enable = true },
-        folds = { enable = true },
-        ensure_installed = {
-            "bash",
-            "html",
-            "css",
-            "javascript",
-            "typescript",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "php",
-            "regex",
-            "toml",
-            "yaml",
-            "vim",
-            "vimdoc",
-            "xml",
-            "go",
-            "gomod",
-        }
-    },
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	opts = {
+		auto_install = true,
+		folds = { enable = true },
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
+		local ensureInstalled = {
+			-- ... your parsers
+			"bash",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"php",
+			"regex",
+			"toml",
+			"yaml",
+			"vim",
+			"vimdoc",
+			"xml",
+			"go",
+			"gomod",
+            "typescript"
+		}
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+		require("nvim-treesitter").install(parsersToInstall)
+	end,
 }
